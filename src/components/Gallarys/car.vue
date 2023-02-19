19027ea1e4db55ecc4292521b0aca8302a890948
Theodora91131 <10556003@ntub.edu.tw>
2023-02-19 21:05:37 +0800

    feat: add plain point

---
+++ src/components/Gallarys/car.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CesiumButtonVue from "../CesiumButton.vue";
+//import { getWidget } from "../../map";
+import { car } from "../../core/car";
+import {  getDefaultEntityCollection } from "../../map";
+let show = false;
+let Car: car;
+//var entity = getDefaultEntityCollection()
+
+const trigger = () => {
+  show = !show;
+  const entities = getDefaultEntityCollection();
+  if (!Car) Car= entities.add(new car());
+  Car.show = show;
+  console.log(Car)
+};
+</script>
+<template>
+  <CesiumButtonVue @click="trigger">car</CesiumButtonVue>
+</template>
